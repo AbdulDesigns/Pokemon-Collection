7428394cfdc3014682c2f1c5cb0f0f97fa1581df
abdul <abduldesigns24@gmail.com>
2025-05-03 13:01:04 +0530

    Update Container component: increase Pokémon fetch limit to 20, adjust background color, and enhance input styling

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -9,7 +9,7 @@ const Container = () => {
   const fetchData = async () => {
     try {
       const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=10"
+        "https://pokeapi.co/api/v2/pokemon?limit=20"
       );
       const data = await response.json();
       console.log(data.results);
@@ -38,7 +38,7 @@ const Container = () => {
   }, []);
 
   return (
-    <div className="bg-amber-900 min-h-screen">
+    <div className="bg-amber-950 min-h-screen">
       <h1 className="text-7xl capitalize font-bold text-center py-15 text-amber-300">
         Pokemon Hunt
       </h1>
@@ -51,7 +51,7 @@ const Container = () => {
           name="search"
           id="search"
           placeholder="Search Pokémon..."
-          className="py-2 text-amber-50 w-1/2 px-4 border border-amber-200 rounded-2xl focus:outline-none focus:ring-2 focus:ring-amber-500 focus:border-transparent"
+          className="py-2 text-amber-50 w-1/2 px-4 border border-amber-200 rounded-2xl focus:outline-none focus:ring-2 focus:ring-amber-500 focus:border-transparent transition duration-300"
         />
         <button
           type="submit"
@@ -62,7 +62,10 @@ const Container = () => {
       </form>
       <ul className="grid w-full mx-auto grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8  p-8 ">
         {pokemonData.map((pokemon) => (
-          <li key={pokemon.id}>
+          <li
+            key={pokemon.id}
+            className="hover:scale-105  hover:z-10 hover:shadow-amber-500/30 hover:shadow-lg  transition duration-300"
+          >
             <Card pokemon={pokemon} />
           </li>
         ))}
