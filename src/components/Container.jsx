08d1b0ef11feb67c07b8ba59d22348d1177d1b77
abdul <abduldesigns24@gmail.com>
2025-05-03 07:29:39 +0530

    Refactor fetchData function to include error handling and improve readability

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,13 +3,28 @@ import Card from "./Card";
 
 const Container = () => {
   // This component serves as a container for the Card component
+  // and is responsible for fetching data from the PokeAPI.
   const fetchData = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
-    const data = await response.json();
-    console.log(data.results);
-    // console.log(data.results[0].url);
-  };
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=10"
+      );
+      const data = await response.json();
+      console.log(data.results);
+      // console.log(data.results[0].url);
 
+      //now we need to get data from the url using async await in the map function
+      const detailedData = data.results.map(async (cur) => {
+        const res = await fetch(cur.url);
+        const resData = await res.json();
+        console.log(resData);
+        return resData;
+      });
+      console.log(detailedData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
   //getting data from the API
   useEffect(() => {
     fetchData();
